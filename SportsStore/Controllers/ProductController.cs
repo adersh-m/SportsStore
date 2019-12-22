4ab71dacbccfe26448b313bdd72a27965808d4f7
Adersh M <adersh.008@gmail.com>
2019-12-22 07:50:06 +0000

    My First asp.net core project

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Contracts;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string category, int productPage = 1)
+        {
+            var vm = new ProductsListViewModel()
+            {
+                Products = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PageInfo = new PageInfo()
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems =category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(x=>x.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ SportsStore/Models/EfProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EfProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EfProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
